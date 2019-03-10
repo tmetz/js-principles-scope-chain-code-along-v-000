813e5f739e3aab9a0211d7897ec2d3f4f22a81e5
Tammy Metz <tammy@tammymetz.com>
2019-03-10 12:15:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'me';
+}
+
+const leastFavoriteCustomer;
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'me';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'bob';
+  const favoriteCustomer = 'me';
+}
